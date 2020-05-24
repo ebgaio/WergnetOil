80b0ae5c32ec364cd6406740fceb59ed4d3bff47
Evandro Barroso Gaio <ebgaio@gmail.com>
2020-05-25 00:27:51 +0100

    Clean import in class.

--- wergnet-oil/src/main/java/com/wergnet/wergnetoil/repository/CustomerRepository.java
+++ wergnet-oil/src/main/java/com/wergnet/wergnetoil/repository/CustomerRepository.java
@@ -3,7 +3,6 @@ package com.wergnet.wergnetoil.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wergnet.wergnetoil.model.Customer;
-import com.wergnet.wergnetoil.repository.card.CardRepositoryQuery;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
