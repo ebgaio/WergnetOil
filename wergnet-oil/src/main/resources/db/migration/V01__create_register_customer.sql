885b9d9a9be3ea097e6dd541ce3f47ba8fe9be25
Evandro Barroso Gaio <ebgaio@gmail.com>
2021-01-11 17:19:12 +0000

    - Change " to ' when words used in DDL files. - Change decimal values from 10.2 to 10,2

--- wergnet-oil/src/main/resources/db/migration/V01__create_register_customer.sql
+++ wergnet-oil/src/main/resources/db/migration/V01__create_register_customer.sql
@@ -13,9 +13,9 @@ CREATE TABLE CUSTOMER (
     country VARCHAR(30)
 )ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
 
-INSERT INTO CUSTOMER (name_customer, last_name, active, email, phone, street1, street2, county, city, eir_code, country) VALUES ('Evandro', 'Gaio', true, 'evandro@gmail.com', '3583125066', "5 OConnel", "Centre", "Dublin", "Dublin", "D6W", "Ireland");
-INSERT INTO CUSTOMER (name_customer, last_name, active, email, phone, street1, street2, county, city, eir_code, country) VALUES ('Andressa', 'Finotti', true, 'andressa@gmail.com', '553732164272', "5 Port", "Donegal", "Dublin", "Dublin", "D6W", "Ireland");
-INSERT INTO CUSTOMER (name_customer, last_name, active, email, phone, street1, street2, county, city, eir_code, country) VALUES ('Ana', 'Gois', true, 'ana@uol.com.br', '553432456874', "5 Tirol", "Clonsila", "Morris", "Dublin", "D2", "Ireland");
-INSERT INTO CUSTOMER (name_customer, last_name, active, email, phone, street1, street2, county, city, eir_code, country) VALUES ('Paula', 'Mendes', true, 'paula@pop.com.br', '551132654782', "5 Lest", "Centre", "Dublin", "Dublin", "D3", "Ireland");
-INSERT INTO CUSTOMER (name_customer, last_name, active, email, phone, street1, street2, county, city, eir_code, country) VALUES ('Oto', 'Costa', true, 'oto@ibm.com', '553136548521', "5 Kirney", "Centre", "Dublin", "Dublin", "D2", "Ireland");
+INSERT INTO CUSTOMER (name_customer, last_name, active, email, phone, street1, street2, county, city, eir_code, country) VALUES ('Evandro', 'Gaio', true, 'evandro@gmail.com', '3583125066', '5 OConnel', 'Centre', 'Dublin', 'Dublin', 'D6W', 'Ireland');
+INSERT INTO CUSTOMER (name_customer, last_name, active, email, phone, street1, street2, county, city, eir_code, country) VALUES ('Andressa', 'Finotti', true, 'andressa@gmail.com', '553732164272', '5 Port', 'Donegal', 'Dublin', 'Dublin', 'D6W', 'Ireland');
+INSERT INTO CUSTOMER (name_customer, last_name, active, email, phone, street1, street2, county, city, eir_code, country) VALUES ('Ana', 'Gois', true, 'ana@uol.com.br', '553432456874', '5 Tirol', 'Clonsila', 'Morris', 'Dublin', 'D2', 'Ireland');
+INSERT INTO CUSTOMER (name_customer, last_name, active, email, phone, street1, street2, county, city, eir_code, country) VALUES ('Paula', 'Mendes', true, 'paula@pop.com.br', '551132654782', '5 Lest', 'Centre', 'Dublin', 'Dublin', 'D3', 'Ireland');
+INSERT INTO CUSTOMER (name_customer, last_name, active, email, phone, street1, street2, county, city, eir_code, country) VALUES ('Oto', 'Costa', true, 'oto@ibm.com', '553136548521', '5 Kirney', 'Centre', 'Dublin', 'Dublin', 'D2', 'Ireland');
 
